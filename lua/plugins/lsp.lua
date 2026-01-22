47342c4a2ac3205de84a35e6b022c76a5ccf4865
Manuel Arnold <arnoldmanuel77@gmail.com>
2026-01-22 15:28:23 +0100

    add lsp and cmp to our config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,91 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          -- Web / React / TS
+          "ts_ls", -- typescript-language-server
+          "html",
+          "cssls",
+          "jsonls",
+          "tailwindcss",
+
+          -- Go / Python
+          "gopls",
+          "pyright",
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- LSP keymaps (buffer-local)
+      local on_attach = function(_, bufnr)
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+        map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+        map("n", "gr", vim.lsp.buf.references, "References")
+        map("n", "gi", vim.lsp.buf.implementation, "Implementation")
+        map("n", "K", vim.lsp.buf.hover, "Hover docs")
+        map("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
+        map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
+        map("n", "<leader>e", vim.diagnostic.open_float, "Line diagnostics")
+        map("n", "[d", vim.diagnostic.goto_prev, "Prev diagnostic")
+        map("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
+        map("n", "<leader>q", vim.diagnostic.setloclist, "Diagnostics list")
+      end
+
+      -- TypeScript / JavaScript
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- Tailwind CSS
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      -- HTML / CSS / JSON
+      lspconfig.html.setup({ capabilities = capabilities, on_attach = on_attach })
+      lspconfig.cssls.setup({ capabilities = capabilities, on_attach = on_attach })
+      lspconfig.jsonls.setup({ capabilities = capabilities, on_attach = on_attach })
+
+      -- Go / Python
+      lspconfig.gopls.setup({ capabilities = capabilities, on_attach = on_attach })
+      lspconfig.pyright.setup({ capabilities = capabilities, on_attach = on_attach })
+
+      -- Diagnostics UI (nice defaults)
+      vim.diagnostic.config({
+        virtual_text = true,
+        severity_sort = true,
+        float = { border = "rounded" },
+      })
+    end,
+  },
+}
